e55436a002d06fc04564c9add7bf41517b3a66d1
tubbytwins <mschnoor@gmail.com>
2022-12-04 01:15:56 -0800

    Adding more of my custom (and private) macros.

--- keyboards/bumwings/bumwings_common.c
+++ keyboards/bumwings/bumwings_common.c
@@ -7,11 +7,38 @@
  * Handler for custom keycodes.
  */
 
+#define IWAIT SS_DELAY(40)
+
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     switch(keycode) {
         case HEX_0X: // Types "0x" for hexadecimal numbers
             if (record->event.pressed) {
                 SEND_STRING("0x");
+            }
+            break;
+        case MY_BPK: // Types "BPK"
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LSFT) IWAIT "B" IWAIT "P" IWAIT "K" IWAIT SS_UP(X_LSFT) IWAIT);
+            }
+            break;
+        case MY_HCI: // Types "HCI"
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LSFT) IWAIT "H" IWAIT "C" IWAIT "I" SS_UP(X_LSFT) IWAIT);
+            }
+            break;
+        case MY_I3C: // Types "I3C"
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LSFT) IWAIT "I" IWAIT "3" IWAIT "C" SS_UP(X_LSFT) IWAIT);
+            }
+            break;
+        case MY_TCRI: // Types "TCRI"
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LSFT) IWAIT "T" IWAIT "C" IWAIT "R" IWAIT "I" SS_UP(X_LSFT) IWAIT);
+            }
+            break;
+        case MY_S3M: // Types "S3M"
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LSFT) IWAIT "S" IWAIT "3" IWAIT "M" SS_UP(X_LSFT) IWAIT);
             }
             break;
     }
--- keyboards/bumwings/bumwings_common.h
+++ keyboards/bumwings/bumwings_common.h
@@ -9,6 +9,11 @@
  
 enum custom_keycodes {
     HEX_0X = SAFE_RANGE,
+    MY_BPK,
+    MY_HCI,
+    MY_I3C,
+    MY_TCRI,
+    MY_S3M,
 };
 
 
